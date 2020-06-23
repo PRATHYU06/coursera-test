619d71803966561538bbbcbd00ad7687fc81d884
Eswari-Priya <62456635+Eswari-Priya@users.noreply.github.com>
2020-06-23 13:38:04 +0530

    small change

--- module5-assignment/assignment5-solution-starter/js/script.js
+++ module5-assignment/assignment5-solution-starter/js/script.js
@@ -97,20 +97,20 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      html =  homeHtml;
+      
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = "'" + chooseRandomCategory(categories).short_name + "'";
 
-      html = insertProperty(html,randomCategoryShortName,chosenCategoryShortName);
-      html = $dc.loadMenuItems(chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+     
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
       // WARNING! You are inserting something that will have to result in a valid Javascript
-      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
+      // syntax because the substitution of {{randomCategorySortName}} becomes an argument
       // being passed into the $dc.loadMenuItems function. Think about what that argument needs
       // to look like. For example, a valid call would look something like this:
       // $dc.loadMenuItems('L')
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-       insertHtml("#main-content",html);
+       insertHtml("#main-content",homeHtmlToInsertIntoMainPage,html);
   
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
